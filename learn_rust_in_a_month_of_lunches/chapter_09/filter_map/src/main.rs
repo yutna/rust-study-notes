247a5899eec0c85933660c18697b656459fa3437
Yutthana Siphuengchai <yutthana.s@outlook.com>
2024-12-17 03:01:46 +0700

    Add chapter 9 examples

---
+++ learn_rust_in_a_month_of_lunches/chapter_09/filter_map/src/main.rs
@@ -0,0 +1,38 @@
+struct Company {
+    name: String,
+    ceo: Option<String>,
+}
+
+impl Company {
+    fn new(name: &str, ceo: &str) -> Self {
+        let ceo = match ceo {
+            "" => None,
+            ceo => Some(ceo.to_string()),
+        };
+
+        Self {
+            name: name.to_string(),
+            ceo,
+        }
+    }
+
+    fn get_ceo(&self) -> Option<String> {
+        self.ceo.clone()
+    }
+}
+
+fn main() {
+    let company_vec = vec![
+        Company::new("Umbrella Corporation", "Unknown"),
+        Company::new("Ovintiv", "Brendan McCracken"),
+        Company::new("The Red-Headed League", ""),
+        Company::new("Stark Enterprises", ""),
+    ];
+
+    let all_the_ceos = company_vec
+        .iter()
+        .filter_map(|company| company.get_ceo())
+        .collect::<Vec<String>>();
+
+    println!("{:?}", all_the_ceos);
+}
---
+++ learn_rust_in_a_month_of_lunches/chapter_09/ok_or_else_method/src/main.rs
@@ -0,0 +1,51 @@
+struct Company {
+    name: String,
+    ceo: Option<String>,
+}
+
+impl Company {
+    fn new(name: &str, ceo: &str) -> Self {
+        let ceo = match ceo {
+            "" => None,
+            ceo_name => Some(ceo_name.to_string()),
+        };
+
+        Self {
+            name: name.to_string(),
+            ceo,
+        }
+    }
+
+    fn get_ceo(&self) -> Option<String> {
+        self.ceo.clone()
+    }
+}
+
+fn get_current_datetime() -> String {
+    "2024-01-27T23:11:23".to_string()
+}
+
+fn main() {
+    let company_vec = vec![
+        Company::new("Umbrella Corporation", "Unknown"),
+        Company::new("Ovintiv", "Brendan McCracken"),
+        Company::new("The Red-Headed League", ""),
+        Company::new("Stark Enterprises", ""),
+    ];
+
+    let results: Vec<Result<String, String>> = company_vec
+        .iter()
+        .map(|company| {
+            company.get_ceo().ok_or_else(|| {
+                let err_message = format!("No CEO found for {}", company.name);
+                println!("{err_message} at {}", get_current_datetime());
+                err_message
+            })
+        })
+        .collect();
+
+    results
+        .iter()
+        .filter(|res| res.is_ok())
+        .for_each(|res| println!("{res:?}"));
+}
