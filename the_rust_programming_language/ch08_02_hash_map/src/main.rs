c90a363892445b619e3761956cf3d58408b23a4c
Yutthana Siphuengchai <yutthana.s@outlook.com>
2025-02-08 17:39:57 +0700

    Finished chapter 8

---
+++ the_rust_programming_language/ch08_02_hash_map/src/main.rs
@@ -0,0 +1,77 @@
+use std::collections::HashMap;
+
+fn main() {
+    // Creating a new hash map.
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    println!("{:?}", scores);
+
+    // Accessing Values in a Hash Map
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    println!("{score}");
+
+    // Iterate over each key-value pair in a hash-map
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    // Hash Maps and Ownership
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are INVALID at this point.
+
+    // ---
+
+    // Updating a hash map
+    // Overwrite a value
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{scores:?}");
+
+    // Adding a Key and Value Only If a Key is NOT Present
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{scores:?}");
+
+    // Updating a Value Based on the Old Value
+
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{map:?}");
+}
